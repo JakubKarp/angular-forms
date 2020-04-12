033e3acf07a392eac75fa5c7cb99ff0be52dc0f5
j.karp <j.karp@createit.com>
2020-04-12 11:51:12 +0200

    form-controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,40 +10,48 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   // @ViewChild to pozwala na dostęp do form przed submitem
-  // @ViewChild('f') signUpForm: NgForm
+  // @ViewChild('f') signUpForm: NgForm;
 
   defaultSelectValue = 'pet';
+  answer = '';
+  genders = ['male', 'female'];
+  isSubmited = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
 
     // onSubmit(form: HTMLFormElement ) {
-    // form - daje cały dostępo do obiektu form
-  onSubmit(form: NgForm ) {
-    console.log('Form!', form);
-    console.log('Submited!', form.value);
-    console.log('Names!', form.value.name);
-  }
+    // form - daje cały dostępo do obiektu form (założone na tagu form)
+    onSubmit(form: NgForm ) {
+      console.log('Form!', form);
+      console.log('Submited!', form.value);
+      console.log('Names!', form.value.userData.name);
 
+      // aby wszystko skasować
+      form.reset();
+    }
 
   // with ViewChild
   // onSubmit() {
   //   console.log(this.signUpForm);
+
+  // // kasowanie
+  //  this.signUpForm.reset();
   // }
 
 }
 
 
 // w Angularze dwa podejścia do formularzy
-// template-driven - formularz jest wnioskowany/dedukowany z DOM
+// template-driven - formularz jest wnioskowany/dedukowany z DOM - to jest to podejście z gotwym html
 // reactive - formularz programujemuy i sybchronizujemy z DOM - more complicated
 
 // template-driven
 // musimy dodać ngModel do inputa i name
 // i w elemencie form dajemy akcję (ngSubmit)
 // a także dajemy refa i przekazujemy go w funkcji
-// jeśli damy samego refa, bez #f='ngForm - dostaniemy cały obiekt z tagami
+// jeśli damy samego refa, bez #f='ngForm' - dostaniemy cały obiekt z tagami
 // z ngForm też dostajemy objekt, ale z niego możemy wyciągać różne rzeczy
 
 
@@ -59,6 +67,20 @@ export class AppComponent {
 // defaults values
 // [ngModel]="defaultSelectValue"
 // lub [ngModel]="'twój tekst'"
+
+// ngModel - 3 sposoby
+// no binding - ngModel + name - input pod kontrolą
+// one-way binding - [ngModel]="defaultSelectValue" - dla default value
+// two-way binding - [(ngModel)]="answer" - dla output value in other place
+
+// zgrupowanie kilku inputów - do value dodaje obiekt userData, daje dostęp do wszystkich ficzerów z klasami
+// ngModelGroup="userData"
+// a potem #userData="ngModelGroup" na tym samym elemencie co wyżej dajemy refa
+// i możemy się odnosić do klas *ngIf="!userData.valid && userData.touched"
+
+
+// input radio po dodaniu ngModel również wskakuje do value
+
 
 
 
